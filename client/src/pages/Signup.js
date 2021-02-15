a9e936d43d2fca5457f9b9a2c1f931cea46b06b5
uberbrent <bmjohns1337@gmail.com>
2021-02-15 10:43:04 -0600

    added tailwind css styling

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -3,7 +3,31 @@ import { Link } from "react-router-dom";
 import { useMutation } from '@apollo/react-hooks';
 
 function Signup() {
-
+    return(
+        <div className="main">
+            <form className="form">
+                <h1>SIGN UP</h1>
+                <div className="textfields">
+                    <div>
+                        <input type="text" name="firstName" />
+                    </div>
+                    <div>
+                        <input type="text" name="lastName" />
+                    </div>
+                    <div>
+                        <input type="email" name="email" />
+                    </div>
+                    <div>
+                        <input type="text" name="password" />
+                    </div>
+                    <div>
+                        <input type="text" name="rePassword" />
+                    </div>
+                    <button type="submit">Sign Up!</button>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default Signup;
