1772450a056ee6d2aab7a3fa9dea9dae9f034780
In Code We Trust <njderenne@gmail.com>
2021-02-15 09:16:24 -0600

    added Message model and added friends to User model

---
+++ server/models/Message.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+
+const messageSchema = new Schema(
+    {
+        messageText: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp)
+        },
+        email: {
+            type: String,
+            required: true
+        }
+    }
+)
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
