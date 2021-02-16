789172d35f4eaf382fb73849ddd4c37b6697c83c
uberbrent <bmjohns1337@gmail.com>
2021-02-16 15:40:36 -0600

    added style and jxml for sidebar component

---
+++ client/src/Components/Sidebar/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+function Sidebar() {
+    return (
+        <div classname="bg-yellow-200 bg-transparent relative">
+            <div className="border-b-4 border-black">
+                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Conversations</h2>
+            </div>
+            <div className="grid mx-auto justify-center grid-flow-row">
+                <div className="flex hover:bg-yellow-400 my-1">
+                    <img src="../../../public/avatar.png" default />
+                    <p className="text-lg font-bold text-gray-900">Brent Johnson</p>
+                </div>
+                <div className="flex hover:bg-yellow-400 my-1">
+                    <img src="../../../public/avatar.png" default />
+                    <p className="text-lg font-bold text-gray-900">Joe Soltis</p>
+                </div>
+                <div className="flex hover:bg-yellow-400 my-1">
+                    <img src="../../../public/avatar.png" default />
+                    <p className="text-lg font-bold text-gray-900">Frank Eslinger</p>
+                </div>
+                <div className="flex hover:bg-yellow-400 my-1">
+                    <img src="../../../public/avatar.png" default />
+                    <p className="text-lg font-bold text-gray-900">Rachel Pampel</p>
+                </div>
+                <div className="flex hover:bg-yellow-400 my-1">
+                    <img src="../../../public/avatar.png" default />
+                    <p className="text-lg font-bold text-gray-900">Mat Lynaugh</p>
+                </div>
+                <div className="flex hover:bg-yellow-400 my-1">
+                    <img src="../../../public/avatar.png" default />
+                    <p className="text-lg font-bold text-gray-900">Andrew Smajda</p>
+                </div>
+                <div className="flex hover:bg-yellow-400 my-1">
+                    <img src="../../../public/avatar.png" default />
+                    <p className="text-lg font-bold text-gray-900">Ben Kohlman, Andy Harris</p>
+                </div>
+            </div>
+            <div className="absolute bottom-0 border-t-4 border-black">
+                <div className="flex">
+                    <img src="../../../public/avatar.png" default />
+                    <p>Test User One</p>
+                </div>
+                <div>
+                    <p>Status: Online</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Sidebar from '../Components/Sidebar'
 
 
 const Dashboard = () => {
     return (
-        <div>
-            <h1>wordUp</h1>
+        <div className="grid grid-cols-4">
+            <div className="col-span-2 col-start-1 col-end-4">
+                <h1>wordUp</h1>
+            </div>
+            <div className="col-span-1 col-start-4 col-end-5 border-l-2 border-black h-screen bg-yellow-200 bg-transparent">
+                <Sidebar />
+            </div>
         </div>
     )
 };
