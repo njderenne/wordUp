a37ba2c064319a0baaec45c82ba758634667e290
uberbrent <bmjohns1337@gmail.com>
2021-02-17 09:50:55 -0600

    added button for new convernsation

--- client/src/Components/Sidebar/index.js
+++ client/src/Components/Sidebar/index.js
@@ -6,6 +6,11 @@ function Sidebar() {
             <div className="border-b-4 border-black">
                 <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Conversations</h2>
             </div>
+            <div className="">
+                <button className="w-10/12 mx-auto my-3 flex items-center justify-center px-8 border border-transparent text-base font-medium rounded-md text-white bg-green-500 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                     + New Conversation
+                </button>
+            </div>
             <div className="grid mx-auto justify-center grid-flow-row">
                 <div className="flex hover:bg-yellow-400 my-1">
                     <img src="../../../public/avatar.png" default />
@@ -36,11 +41,16 @@ function Sidebar() {
                     <p className="text-lg font-bold text-gray-900">Ben Kohlman, Andy Harris</p>
                 </div>
             </div>
-            <div className="fixed container bottom-0 w-full border-t-4 border-black p-6">
-                <div className="mx-auto justify-center">
-                    <img className="float-right" src="../../../public/avatar.png" default />
+            <div className="fixed container bottom-0 w-full border-t-4 border-black p-6 grid grid-cols-2">
+                <div className="col-auto">
+                    <img className="" src="../../../public/avatar.png" default />
                     <p className="text-lg font-bold text-gray-900">Test User One</p>
                     <p className="text-lg font-bold text-gray-900">Status: Online</p>
+                </div>
+                <div className="col-auto">
+                    <p>
+                        Logout
+                    </p>
                 </div>
             </div>
         </div>
