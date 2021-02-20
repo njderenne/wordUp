2a4a76e09e9d1f1530ca52a43c77072020aa33fd
In Code We Trust <njderenne@gmail.com>
2021-02-20 15:15:49 -0600

    changed background color

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -6,7 +6,7 @@ import Conversation from '../Components/Conversation'
 const Dashboard = () => {
     return (
         <div className="grid grid-cols-4 grid-flow-row">
-            <div className="col-span-2 col-start-1 col-end-4 h-screen">
+            <div className="col-span-2 col-start-1 col-end-4 h-screen bg-gray-800">
                 <Conversation />
             </div>
             <div className="col-span-1 col-start-4 col-end-5 border-l-2 border-black h-screen bg-yellow-200 bg-transparent">
