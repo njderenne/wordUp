3ef4ae8f13e6c82b144c7193ffca3de914bc2837
In Code We Trust <njderenne@gmail.com>
2021-02-26 17:40:42 -0600

    reverted back to http

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const { SubscriptionServer } = require('subscriptions-transport-ws');
 
 const { makeExecutableSchema } = require('graphql-tools');
 const { execute, subscribe } = require('graphql');
-const { createServer } = require('https');
+const { createServer } = require('http');
 const schema = makeExecutableSchema({
     typeDefs: typeDefs,
     resolvers: resolvers,
