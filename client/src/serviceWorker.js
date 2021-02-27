66961a1ae48290d33569bb79593015c03f5beb21
In Code We Trust <njderenne@gmail.com>
2021-02-26 20:13:00 -0600

    updated swURL in SW

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -23,8 +23,7 @@ const isLocalhost = Boolean(
       }
   
       window.addEventListener('load', () => {
-        console.log(process.env.PUBLIC_URL);
-        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+        const swUrl = `https://fast-shelf-56121.herokuapp.com/service-worker.js`;
   
         if (isLocalhost) {
           // This is running on localhost. Let's check if a service worker still exists or not.
