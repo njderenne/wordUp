1e828a46f351d97a64f2fc462f2e66060fca212b
In Code We Trust <njderenne@gmail.com>
2021-02-28 15:28:06 -0600

    updated index.js sw import

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 import './tailwind.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
