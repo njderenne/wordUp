f7219d227ea5a4a560050179e2d85b4f7706c35d
In Code We Trust <njderenne@gmail.com>
2021-02-28 12:48:33 -0600

    moved sw to public directory

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 import './tailwind.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from '../public/serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
