cbd3822eeb74f4a64a321297d890d8b8235a34dc
In Code We Trust <njderenne@gmail.com>
2021-02-28 16:56:04 -0600

    updated service worker

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 import './tailwind.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,4 +16,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-serviceWorker.register();
+serviceWorkerRegistration.register();
